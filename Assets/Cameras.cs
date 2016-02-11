aa2ececfb0e215abc0254a6687d9fa457d4aad4b
Ayaminn <ayamin.imai@gmail.com>
2016-02-11 14:17:55 +0900

    First Commit

---
+++ Assets/Cameras.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameras : MonoBehaviour {
+	Camera leftCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion q = Input.gyro.attitude;
+		Quaternion qq = Quaternion.AngleAxis(-90.0f, Vector3.left);
+		q.x *= -1.0f;
+		q.y *= -1.0f;
+		this.transform.localRotation = qq * q;
+	}
+}
